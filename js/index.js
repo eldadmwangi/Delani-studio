8e612372a9a0d4313e9c1736f330f5b6cd644a8d
eldadmwangi <eldadmwangi@gmail.com>
2020-05-18 01:57:56 +0300

    more

--- js/index.js
+++ js/index.js
@@ -18,4 +18,24 @@ $(document).ready(function(){
           $(".prod").click(function(){
               $(".product").toggle();
             });
-      });
+
+            $("#message").click(function(event) {
+                whatToSay = "Hi, we have recieved your message";
+                alert(whatToSay);
+              });
+
+            $( "#portfolios" ).hover(
+                function() {
+                  $( this ).append( $( "<span> ***</span>" ) );
+                }, function() {
+                  $( this ).find( "span" ).last().remove();
+                });
+
+                $( "#portfolios.img" ).hover(function() {
+                    $( this ).fadeOut( 100 );
+                    $( this ).fadeIn( 500 );
+                  });
+            });
+
+
+
